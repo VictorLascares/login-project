7bb1e330077dae444cf2de44c4c7a38a1899779d
Victor Lascares <vmlascares@gmail.com>
2021-11-30 01:15:43 -0600

    Agregando funcionalidad de generar una compra

---
+++ app/Models/Compra.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Compra extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cantidad','pago','ticket','calificacion','estado','user_id','product_id'];
+}
